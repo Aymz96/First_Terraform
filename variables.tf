7b1cc1e9824fbc66ec8e9557950a17a3fe6170bc
Aymz96 <ayman2k13@hotmail.co.uk>
2020-04-24 19:37:22 +0100

    Intro to terraform, Creating EC2, VPC & AMI

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_id" {
+  default = "vpc-07e47e9d90d2076da"
+}
+
+variable "name" {
+  default = "Aymz_Terraform_App_2"
+}
+
+variable "ami_id" {
+  default = "ami-0bceebcfc0c12d935"
+}
